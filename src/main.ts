10a5a44929805f65cb435a35740c1802509cd528
Sepideh Chaichi <s.chaichi@charisma.tech>
2025-06-02 10:39:56 +0330

    register sw in production mode-main.ts file

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,12 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+  if(import.meta.env.PROD){
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      const registration = await navigator.serviceWorker.register(`${import.meta.env.BASE_URL}service-worker.js`)
+      registration.update();
+    })
+  }
+}
