906c6256a00d91c3add6cccdca3258c82d469159
Sepideh Chaichi <s.chaichi@charisma.tech>
2025-06-02 10:36:22 +0330

    change service worker to register sw

--- src/sw.ts
+++ src/sw.ts
@@ -1,12 +1,18 @@
 /// <reference lib="webworker" />
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 import { clientsClaim } from 'workbox-core'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { registerRoute } from 'workbox-routing'
+import { StaleWhileRevalidate } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
+const CACHE_ALLOWLIST = ['workbox-precache', 'svg-cache', 'js-cache', 'css-cache']
 
 declare let self: ServiceWorkerGlobalScope
 
 // self.__WB_MANIFEST is the default injection point
 precacheAndRoute(self.__WB_MANIFEST)
+cleanupOutdatedCaches()
+clientsClaim()
+
 
 // clean old assets
 cleanupOutdatedCaches()
@@ -17,10 +23,32 @@ if (import.meta.env.DEV)
   allowlist = [/^\/$/]
 
 // to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
-))
+registerRoute(
+  ({ url }) => url.pathname.endsWith('.svg') ,
+  new StaleWhileRevalidate({
+    cacheName: 'svg-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 30 * 24 * 60 * 60, 
+      }),
+    ],
+  }),
+)
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
 
-self.skipWaiting()
-clientsClaim()
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (!CACHE_ALLOWLIST.some((prefix) => key.startsWith(prefix))) {
+            return caches.delete(key)
+          }
+        })
+      )
+    ).then(() => self.clients.claim())
+  )
+})
